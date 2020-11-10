57431165f03d328ece1688a33367f4cf36ced857
Даяна Тей <ditey@edu.hse.ru>
2020-11-10 23:12:05 +0300

    is editted

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -31,8 +31,21 @@ namespace Task05
             int N = int.Parse(Console.ReadLine());
 
             // TODO: объявите массив и вызовите метод для его заполнения
-            
+            int[] array = new int[N];
+            Result(ref array);
+
             // TODO: выведите массив на экран в требуемом порядке
+            for (int count = N - 1; count >= 0; --count)
+                Console.Write(array[count]);
+            Console.Write("\n");
+        }
+
+        public static void Result(ref int[] array)
+        {
+            array[0] = 1;
+            array[1] = 1;
+            for (int elem = 2; elem < array.Length; ++elem)
+                array[elem] = array[elem - 1] + array[elem - 2];
         }
     }
 }
